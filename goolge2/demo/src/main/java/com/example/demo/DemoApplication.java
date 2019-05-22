c4e98badcef4f86c638ba6e9c315d090bccb4188
poo1130 <yugioh9@naver.com>
2019-05-23 00:10:10 +0900

    googleOAuthTESTCode_v1.0

--- goolge2/demo/src/main/java/com/example/demo/DemoApplication.java
+++ goolge2/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final String PROPERTIES = "spring.config.location=classpath:/google.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		new SpringApplicationBuilder(DemoApplication.class)
+				.properties(PROPERTIES)
+				.run(args);
 	}
 
 }
