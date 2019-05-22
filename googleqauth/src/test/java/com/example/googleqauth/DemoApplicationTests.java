c9bcc692e18c26f32615ffe3b08221b88c6b5524
poo1130 <yugioh9@naver.com>
2019-05-22 21:53:58 +0900

    update

--- googleqauth/src/test/java/com/example/googleqauth/DemoApplicationTests.java
+++ googleqauth/src/test/java/com/example/googleqauth/DemoApplicationTests.java
@@ -1,16 +1,36 @@
 package com.example.googleqauth;
 
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+// 추가
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import io.restassured.RestAssured;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Before
+	public void setup()
+	{
+		RestAssured.port = 8080;
 	}
+    @Test
+    public void 기본path로_접근하면_index_html_호출된다 () throws Exception {
+        given()
+                .when()
+                    .get("/")
+                .then()
+                    .statusCode(200)
+                    .contentType("text/html")
+                    .body(containsString("권한 관리"));
+    }
 
 }
