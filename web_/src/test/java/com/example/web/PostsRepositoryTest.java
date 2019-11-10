7114a6ae29041d2f66bbc7c364b9e53d5ae4dccd
poo1130 <yugioh9@naver.com>
2019-11-10 18:52:47 +0900

    Update PostsRepositoryTest.java

--- web_/src/test/java/com/example/web/PostsRepositoryTest.java
+++ web_/src/test/java/com/example/web/PostsRepositoryTest.java
@@ -34,6 +34,10 @@ public class PostsRepositoryTest
     public void cleanup()
     {
         postsRepository.deleteAll();
+        /*
+        이후 테스트 코드에 영향을 끼치지 않기 위해 
+        테스트 메소드가 끝날때 마다 respository 전체 비우는 코드
+        */
     }
 
     @Test
